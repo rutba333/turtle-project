51bd76df9cd87c39b6269361c5d8e22b0d913b86
rutba333 <sa2497589@gmail.com>
2024-10-02 23:01:27 +0600

    hello

---
+++ square.py
@@ -0,0 +1,16 @@
+import turtle #importing library
+turtle.Screen().bgcolor("orange")
+turtle.Screen().setup(300,400)
+board=turtle.Turtle()
+board.left(90)
+board.forward(120) 
+
+board.right(90)
+board.forward(120)
+
+board.right(90)
+board.forward(120) 
+
+board.left(90)
+board.backward(120)
+turtle.done()
